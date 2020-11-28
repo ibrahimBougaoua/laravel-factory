5a79233373ef6c3f1746698953587967c5400dca
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-11-28 13:24:58 -0800

    add admin panel

---
+++ resources/views/layouts/panel.blade.php
@@ -0,0 +1,9 @@
+            @include('panel.base.header')
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+                <!-- /.container-fluid -->
+
+            @include('panel.base.footer')
---
+++ routes/panel.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Panel Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => 'panel'], function () {
+       Route::get('/','App\Http\Controllers\Panel\DashbaordController@index')->name('panel.dashboard');
+});
