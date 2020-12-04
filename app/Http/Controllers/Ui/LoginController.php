d52c5daeea6699cdd6ef809cb7787b8078588d59
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-12-04 14:08:09 -0800

    add customer login

---
+++ app/Http/Controllers/Ui/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Ui;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Ui\LoginRequest;
+use App\Models\Ui\Customer;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function checkLogin(LoginRequest $request)
+    {
+        $remembre_me = $request->has('remembre_me') ? true : false;
+    	if (auth()->guard('admin')->attempt(['email' => $request->input("email"),'password' => $request->input("password")],$remembre_me)) {
+    		return redirect()->route('panel.dashboard');
+    	}
+    	return redirect()->back()->with(['error' => 'error login']);
+    }
+
+    public function getLogin()
+    {
+        return view('ui.auth.login');
+    }
+
+    public function logOut()
+    {
+        Auth::logout();
+        return view('ui.auth.login');
+    }
+}
