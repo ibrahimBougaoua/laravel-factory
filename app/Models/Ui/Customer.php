ebe11f6c3a44bf823d6edef7b2691c7686ac213d
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-12-04 14:27:08 -0800

    add Customer

---
+++ app/Models/Ui/Customer.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models\Ui;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $table = "customers";
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'gender',
+        'phone',
+        'city',
+        'address',
+        'photo',
+        'credit_card',
+        'credit_card_type',
+        'billin_address',
+        'billin_city',
+        'billin_region',
+        'billin_postal_code',
+        'black_list',
+        'online',
+        'status',
+        'trash'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
+
+    public function getPhotoAttribute($value)
+    {
+        if ( ! empty($value) )
+            return asset('assets/' . $value);
+        return asset('admin/images/profile.jpg');
+    }
+
+    public function getStatusAttribute($value)
+    {
+        if ( $value == 0 )
+            return 'Active';
+        return 'deactivated';
+    }
+}
