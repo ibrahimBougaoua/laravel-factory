e31983b2b63e3bcf779ad4ff16e436173e3f9111
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-12-05 05:40:35 -0800

    Refactoring : order

---
+++ resources/views/ui/order/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.ui')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            @foreach($carts as $cart)
+            <div class="card mb-3">
+                <div class="card-body">
+                   
+                    <table class="table table-striped mt-2 mb-2">
+                        <thead>
+                            <tr>
+                                
+                                <th scope="col">Title</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($cart->items as $item)
+                            <tr>
+                                
+                                <td>{{$item['name'] }}</td>
+                                <td>${{$item['unit_price'] }}</td>
+                                <td>{{$item['qty'] }}</td>
+                                <td> Paid</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                  
+                </div>
+            </div>
+            <p class="badge badge-pill badge-info mb-3 p-3 text-white">Total Price : ${{$cart->totalPrice}}</p>
+            @endforeach
+        </div>
+    </div>
+</div>
+
+@endsection('content')
