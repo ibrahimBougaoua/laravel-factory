eb5d7d07fdcb53963cd44e5db8f0beb61b997e54
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2020-12-29 20:22:07 +0100

    Refactoring : login

--- resources/views/ui/auth/login.blade.php
+++ resources/views/ui/auth/login.blade.php
@@ -2,37 +2,29 @@
 
 @section('content')
 
-  @include('ui.base.jumbotron')
+  @include('ui.base.jumbotron',['title' => 'Log in to you\'r account.'])
 
   <!--Main layout-->
-  <main class="mt-5 pt-4">
+  <main class="m-5">
 
     @include('ui.alerts.success')
     @include('ui.alerts.errors')
 
-    <div class="container dark-grey-text mt-5">
-
-        <!-- Outer Row -->
+    <div class="container dark-grey-text">
         <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="col-md-8">
+                <div class="card o-hidden border-0 shadow">
                     <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6">
-                                <div class="p-5">
+                            <div class="col-lg-12 p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back !</h1>
                                     </div>
-
-
                                <form class="form-horizontal form-simple" action="{{route('ui.login')}}" method="post">
                                 @csrf
                                 <fieldset class="form-group position-relative has-icon-left mb-0">
-                                    <input type="text" name="email" class="form-control form-control-lg input-lg"
-                                           value="{{old('email')}}" id="email" placeholder="أدخل البريد الالكتروني ">
+                                    <input type="text" name="email" class="form-control form-control-lg input-lg mb-3"
+                                           value="{{old('email')}}" id="email" placeholder="Enter you'r email">
                                     <div class="form-control-position">
                                         <i class="ft-user"></i>
                                     </div>
@@ -57,22 +49,24 @@
                                         <fieldset>
                                             <input type="checkbox" name="remember_me" id="remember-me"
                                                    class="chk-remember">
-                                            <label for="remember-me">تذكر دخولي</label>
+                                            <label for="remember-me">Remeber me</label>
                                         </fieldset>
                                     </div>
                                 </div>
-                                <button type="submit" class="btn btn-info btn-lg btn-block"><i class="ft-unlock"></i>
-                                    دخول
-                                </button>
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <button type="submit" class="btn btn-info btn-lg float-right"><i class="ft-unlock"></i>
+                                            Log in
+                                        </button>
+                                    </div>
+                                </div>
                             </form>
-
-
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="{{ route('ui.customer.getEmail') }}">Forgot Password?</a>
+                                        <a class="small" href="{{ route('ui.customer.getEmail') }}">Forgot Password ?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="{{ route('ui.signup') }}">Create an Account !</a>
                                     </div>
                                 </div>
                             </div>
--- resources/views/ui/base/jumbotron.blade.php
+++ resources/views/ui/base/jumbotron.blade.php
@@ -1,8 +1,8 @@
 <div class="jumbotron" style="background-color: #3F51B5;">
     <div class="container">
-        <div class="row m-0 p-0 justify-content-center">
-            <div class="col-md-8">
-                <h1 class="display-5 text-white font-weight-bold">Log in to you'r account.</h1>
+        <div class="row">
+            <div class="col-md-8 ml-auto">
+                <h1 class="display-5 text-white font-weight-bold">{{ $title }}</h1>
             </div>
         </div>
     </div>
