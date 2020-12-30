0b6c83416183d0777f0221a91820bae4abaddb73
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2020-12-30 12:28:39 +0100

    Refactoring : Orders

--- resources/views/ui/order/index.blade.php
+++ resources/views/ui/order/index.blade.php
@@ -2,19 +2,25 @@
 
 @section('content')
 
+  @include('ui.base.jumbotron',['title' => 'All you\'r orders'])
+
   <!--Main layout-->
-  <main class="mt-5 pt-4">
-    <div class="container dark-grey-text mt-5">
+  <main class="m-5">
+    <div class="container dark-grey-text">
     <div class="row my-2">
-        <div class="col-md-9">
+
+        <div class="col-md-12 p-5 text-muted text-center">
+            <h4 class="h4">All you'r orders</h4>
+        </div>
+        <div class="col-md-12">
             @foreach($carts as $cart)
             <div class="card mb-3">
                 <div class="card-body">
-                   
+
                     <table class="table table-striped mt-2 mb-2">
                         <thead>
                             <tr>
-                                
+
                                 <th scope="col">Title</th>
                                 <th scope="col">Price</th>
                                 <th scope="col">Quantity</th>
@@ -24,7 +30,7 @@
                         <tbody>
                         @foreach($cart->items as $item)
                             <tr>
-                                
+
                                 <td>{{$item['name'] }}</td>
                                 <td>${{$item['unit_price'] }}</td>
                                 <td>{{$item['qty'] }}</td>
@@ -33,10 +39,10 @@
                             @endforeach
                         </tbody>
                     </table>
-                  
+
                 </div>
             </div>
-            <p class="badge badge-pill badge-info mb-3 p-3 text-white">Total Price : ${{$cart->totalPrice}}</p>
+            <p class="badge badge-pill btn-primary mb-3 p-3 text-white">Total Price : ${{$cart->totalPrice}}</p>
             @endforeach
         </div>
     </div>
